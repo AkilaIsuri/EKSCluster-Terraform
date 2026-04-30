c05cba74cbb755b3bfa35dc8ee34e32f529b12c6
Akila Isuri <akilaisuri98@gmail.com>
2026-04-30 22:46:56 +0530

    Initial commit

---
+++ modules/vpc/main.tf
@@ -0,0 +1,10 @@
+#custome vpc modules for EKS
+resource "aws_vpc" "eks_vpc" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.cluster_name}-vpc"
+  }
+}
---
+++ modules/vpc/variables.tf
@@ -0,0 +1,11 @@
+variable "vpc_cidr" {
+    description = "CIDR block for the VPC"
+    type        = string
+  
+}
+
+variable "cluster_name" {
+    description = "Name of the EKS cluster"
+    type        = string        
+  
+}
