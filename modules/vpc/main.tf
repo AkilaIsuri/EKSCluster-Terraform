3e82c40e07bf9a727c8815fff1449f5de2bca048
Akila Isuri <akilaisuri98@gmail.com>
2026-05-01 12:28:05 +0530

    Creating the VPC module

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,10 +1,147 @@
 #custome vpc modules for EKS
-resource "aws_vpc" "eks_vpc" {
+resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
-  tags = {
-    Name = "${var.cluster_name}-vpc"
-  }
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.name_prefix}-vpc"
+    }
+  )
+}
+
+
+#Public subnets
+resource "aws_subnet" "public" {  
+  count             = length(var.public_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.public_subnets[count.index] # Adjust subnet mask as needed
+  availability_zone = var.azs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = merge(
+    var.tags,
+    var.public_subnet_tags,
+    {
+      Name = "${var.name_prefix}-public-${var.azs[count.index]}"
+      Type = "public"
+    }
+  )
+}
+
+#Private subnets
+resource "aws_subnet" "public" {  
+  count             = length(var.private_subnets)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnets[count.index] # Adjust subnet mask as needed
+  availability_zone = var.azs[count.index]
+
+  tags = merge(
+    var.tags,
+    var.private_subnet_tags,
+    {
+      Name = "${var.name_prefix}-private-${var.azs[count.index]}"
+      Type = "private"
+    }
+  )
+}
+#Internet gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.name_prefix}-igw"
+    }
+    )
+}
+
+#Elastic IP for NAT gateway
+resource "aws_eip" "nat" {
+  count = var.enable_nat_gateway ? (var.single_nat_gateway ? 1 : length(var.public_subnets)):0
+  domain = "vpc"
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.name_prefix}-nat-eip-${count.index + 1}"
+    }
+  )
+
+  depends_on = [ aws_internet_gateway.main ]
+}
+
+#NAT gateway
+resource "aws_nat_gateway" "main" {
+  count = var.enable_nat_gateway ? (var.single_nat_gateway ? 1 : length(var.pubic_subnets)):0
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.public[0].id
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.name_prefix}-nat-${count.index + 1}"
+    }
+  )
+
+  depends_on = [aws_internet_gateway.main]
+}
+
+#Route table for public subnets
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.name_prefix}-public-rt"
+    }
+  )
+}
+
+
+#Public route to internet gateway
+resource "aws_route" "public_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id            = aws_internet_gateway.main.id
+  depends_on = [aws_internet_gateway.main]
+}
+
+#Public route table association
+resource "aws_route_table_association" "public" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+#Private route table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+  count = var.single_nat_gateway ? 1 : length(var.private_subnets)
+
+  tags = merge(
+    var.tags,
+    {
+      Name = "${var.name_prefix}-private-rt-${count.index + 1}"
+    }
+  )
+}
+
+#Private route to NAT gateway
+resource "aws_route" "private_nat" {
+  count = var.enable_nat_gateway ? (var.single_nat_gateway ? 1 : length(var.private_subnets)):0
+  route_table_id         = var.single_nat_gateway ? aws_route_table.private[0].id : aws_route_table.private[count.index].id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id        = aws_nat_gateway.main[count.index].id
+}
+
+#Private route table association
+resource "aws_route_table_association" "private" {
+  count          = length(var.private_subnets)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = var.single_nat_gateway ? aws_route_table.private[0].id : aws_route_table.private[count.index].id
 }
--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -4,8 +4,53 @@ variable "vpc_cidr" {
   
 }
 
-variable "cluster_name" {
-    description = "Name of the EKS cluster"
+variable "name_prefix" {
+    description = "Prefix for naming resources in the VPC (e.g., 'dev', 'prod') "
     type        = string        
   
+}
+
+variable "tags" {
+    description = "Tags to apply to all resources"
+    type        = map(string)
+    default     = {}
+}
+
+variable "public_subnets" {
+    description = "List of public subnet cidr blocks"
+    type        = list(string)
+}
+
+variable "private_subnets" {
+    description = "List of private subnet cidr blocks"
+    type        = list(string)
+}
+
+variable "azs" {
+    description = "List of availability zones for subnets"
+    type        = list(string)
+}
+
+variable "public_subnet_tags" {
+    description = "Additional tags for public subnets"
+    type        = map(string)
+    default     = {}
+}
+
+variable "private_subnet_tags" {
+    description = "Additional tags for private subnets"
+    type        = map(string)
+    default     = {}
+}
+
+variable "enable_nat_gateway" {
+    description = "Whether to create a NAT Gateway for private subnets"
+    type        = bool
+    default     = true
+}
+
+variable "single_nat_gateway" {
+    description = "Use a single NAT gateway for all the private subnets."
+    type        = bool
+    default     = true
 }
